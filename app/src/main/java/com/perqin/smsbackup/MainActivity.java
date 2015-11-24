ee99785c5a0ae796b15c2eee2db0dbcfbd0e1ac1
perqin <perqinxie@gmail.com>
2015-11-24 20:13:48 +0800

    Test git

--- app/src/main/java/com/perqin/smsbackup/MainActivity.java
+++ app/src/main/java/com/perqin/smsbackup/MainActivity.java
@@ -2,8 +2,10 @@ package com.perqin.smsbackup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
